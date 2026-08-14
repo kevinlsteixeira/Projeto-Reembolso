dbb9684fb21f9053390824d923110b4e05bda491
Kevin Teixeira <kevinlaurentino@gmail.com>
2026-08-14 16:50:58 -0300

    Button components

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,5 @@
 import { Input } from "../components/Input"
+import { Button } from "../components/Button"
 
 export function SignIn() {
     return <form className="w-full flex flex-col gap-4">
@@ -15,5 +16,7 @@ export function SignIn() {
             type="password"
             placeholder="123456"
         />
+
+        <Button>Entrar</Button>
     </form>
 }
