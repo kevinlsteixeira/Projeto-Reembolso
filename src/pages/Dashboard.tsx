55086b1ad94f6b9401f01dbf452aae47c4b01158
Kevin Teixeira <kevinlaurentino@gmail.com>
2026-08-18 16:54:57 -0300

    Dashboard Page components

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,60 @@
+import { useState } from "react"
+
+import { Input } from "../components/Input"
+import { Button } from "../components/Button"
+import { RefundItem } from "../components/RefundItem"
+
+import searchSvg from "../assets/search.svg"
+
+import { CATEGORIES } from "../utils/categories"
+import { formatCurrency } from "../utils/formatCurrency"
+
+const REFUND_EXAMPLE = {
+    id: "123",
+    name: "Kevin",
+    category: "Transporte",
+    amount: formatCurrency(34.5),
+    categoryImg: CATEGORIES["transport"].icon
+}
+
 export function Dashboard() {
-    return <h1>Dashboard</h1>
+    const [name, setName] = useState("")
+
+    function fetchRefunds(e: React.FormEvent) {
+        e.preventDefault()
+
+        console.log(name)
+    }
+
+    return (
+        <div className="bg-gray-500 rounded-xl p-10 md:min-w-3xl">
+            <h1 className="text-gray-100 font-bold text-xl flex-1">
+                Solicitações
+            </h1>
+
+            <form
+                onSubmit={fetchRefunds}
+                className="flex items-center justify-between pb-6 border-b border-gray-400 md:flex-row gap-2 mt-6"
+            >
+                <Input
+                    placeholder="Pesquisar pelo nome"
+                    onChange={(e) => setName(e.target.value)}
+                />
+
+                <Button type="submit" variant="icon">
+                    <img src={searchSvg} alt="Ícone de Busca"
+                        className="w-5" />
+                </Button>
+            </form>
+
+            <div className="mt-6 flex flex-col gap-4 max-h-85.5 overflow-y-scroll">
+                <RefundItem data={REFUND_EXAMPLE} />
+                <RefundItem data={REFUND_EXAMPLE} />
+                <RefundItem data={REFUND_EXAMPLE} />
+                <RefundItem data={REFUND_EXAMPLE} />
+                <RefundItem data={REFUND_EXAMPLE} />
+                <RefundItem data={REFUND_EXAMPLE} />
+            </div>
+        </div>
+    )
 }
