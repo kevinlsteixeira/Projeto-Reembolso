68d2f6295a0b8a710b04e4629d8901e4e7bb372b
Kevin Teixeira <kevinlaurentino@gmail.com>
2026-08-18 11:29:49 -0300

    Form submit

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,13 +1,25 @@
+import { useState } from "react"
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
 
 export function SignIn() {
-    return <form className="w-full flex flex-col gap-4">
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+
+    function onSubmit(e: React.FormElement) {
+        e.preventDefault()
+
+        alert("Enviado!")
+    }
+
+    return <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
         <Input
             required
             legend="E-mail"
             type="email"
             placeholder="seu@email.com"
+            onChange={(e) => setEmail(e.target.value)}
         />
 
         <Input
@@ -15,8 +27,9 @@ export function SignIn() {
             legend="Senha"
             type="password"
             placeholder="123456"
+            onChange={(e) => setPassword(e.target.value)}
         />
 
-        <Button>Entrar</Button>
+        <Button type="submit" isLoading={isLoading}>Entrar</Button>
     </form>
 }
